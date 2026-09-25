e36c85cc64a02e7ac9aaad01beb4416998f1e0d2
mirko143 <miran123125@gmail.com>
2026-09-25 14:35:54 +0200

    wip

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), process.env.VITEST ? [] : [vueDevTools()]],
   server: {
     port: 3001,
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
